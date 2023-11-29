68dcc068784b8c6c6001fb4814b45b222f1dd94e
NicodemusN <niilo.nevala@gmail.com>
2023-11-29 13:02:33 +0200

    Search.js fix 1

--- search.js
+++ search.js
@@ -11,34 +11,56 @@ document.addEventListener("DOMContentLoaded", function () {
     }
   });
 
-    async function fetchPages(query) {
+  async function fetchPages(query) {
     try {
       // Fetch dynamically generated pages.json
       const response = await fetch("pages.json");
       const pages = await response.json();
   
-      console.log("Fetched pages:", pages);
-  
       // Filter pages based on the query
       const filteredPages = pages.filter((page) =>
-        page.title.toLowerCase().includes(query)
+        page.title.toLowerCase().includes(query.toLowerCase())
       );
   
-      console.log("Filtered pages:", filteredPages);
-  
       // Display search results
-      filteredPages.forEach((page) => {
+      searchResults.innerHTML = ""; // Clear previous results
+  
+      if (filteredPages.length === 0) {
         const li = document.createElement("li");
-        const link = document.createElement("a");
-  
-        link.href = page.url;
-        link.textContent = page.title;
-  
-        li.appendChild(link);
+        li.textContent = "No results found.";
         searchResults.appendChild(li);
-      });
+      } else {
+        for (const page of filteredPages) {
+          const li = document.createElement("li");
+          const link = document.createElement("a");
+  
+          // Use the title found inside the HTML file, or fallback to the filename
+          const pageTitle = await getTitleFromHtml(page.url);
+          link.textContent = pageTitle || page.title;
+          link.href = page.url;
+  
+          li.appendChild(link);
+          searchResults.appendChild(li);
+        }
+      }
     } catch (error) {
       console.error("Error fetching or processing pages:", error);
     }
   }
+  
+  // Function to extract the title from an HTML file
+  async function getTitleFromHtml(url) {
+    try {
+      // Fetch the HTML content
+      const response = await fetch(url);
+      const html = await response.text();
+  
+      // Use a regular expression to find the title inside the <title> tag
+      const match = /<title>(.*?)<\/title>/i.exec(html);
+      return match ? match[1].trim() : null;
+    } catch (error) {
+      console.error("Error fetching HTML content:", error);
+      return null;
+    }
+  }  
 });
