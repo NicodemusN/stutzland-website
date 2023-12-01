2b96f681e9380dbe611db79f2e41cb26350cd7b5
NicodemusN <niilo.nevala@gmail.com>
2023-12-01 09:10:01 +0200

    transcription fixed
    - finally functional transcription
    - lyx.txt can be converted to html, first run transcribe.py and then enum-transcribe.py
    - association act finally appropriately displayed

--- transcription/transcribe.py
+++ transcription/transcribe.py
@@ -1,82 +1,52 @@
-def main():
-    input_filename = "lyx.txt"
-    output_filename = "result.html"
+import os
+import re
 
-    # Check if result.html exists, if yes, write it empty
-    try:
-        with open(output_filename, 'w', encoding='utf-8') as empty_file:
-            empty_file.write("")
-    except FileNotFoundError:
-        pass
+def clear_file(file_path):
+    if os.path.exists(file_path):
+        with open(file_path, 'w', encoding='utf-8'):
+            pass
 
-    # Flag to indicate when to start formatting
-    start_formatting = False
+def convert_to_html(input_file, output_file):
+    # Clear the existing HTML file or create a new one
+    clear_file(output_file)
 
-    with open(input_filename, 'r', encoding='utf-8') as file:
-        lines = file.readlines()
+    with open(input_file, 'r', encoding='utf-8') as infile:
+        lines = infile.readlines()
 
-    with open(output_filename, 'a', encoding='utf-8') as output_file:
-        paragraph_started = False
-        for line in lines:
-            # Skip lines until the first heading is encountered
-            if not start_formatting:
-                if len(line) > 0 and line[0].isdigit():
-                    start_formatting = True
-                continue
+    paragraphs = []
+    current_paragraph = ""
 
-            # Check if the line is empty
-            if not line.strip():
-                # If a paragraph was started, end it
-                if paragraph_started:
-                    output_file.write("</p>\n\n")
-                    paragraph_started = False
-                continue
+    for line in lines:
+        line = line.strip()
+        if line:  # Ignore empty lines
+            current_paragraph += line + ' '
+        elif current_paragraph:
+            paragraphs.append(current_paragraph.strip())
+            current_paragraph = ""
 
-            processed_line = process_line(line)
-            # If a paragraph was not started, start it
-            if not paragraph_started:
-                output_file.write("<p>")
-                paragraph_started = True
-            output_file.write(processed_line)
+    # Add the last paragraph
+    if current_paragraph:
+        paragraphs.append(current_paragraph.strip())
 
-        # If a paragraph is open at the end, close it
-        if paragraph_started:
-            output_file.write("</p>\n")
+    with open(output_file, 'a', encoding='utf-8') as outfile:
+        for paragraph in paragraphs:
+            if re.match(r'^\d+ §', paragraph):
+                # Lines starting with an integer or two integers and containing '§'
+                outfile.write('\n' + f'<h3>{paragraph}</h3>\n')
+            elif re.match(r'^\d+', paragraph):
+                # Lines starting with an integer or two integers
+                outfile.write('\n' + f'<h2>{paragraph}</h2>\n')
+            else:
+                # All other paragraphs
+                outfile.write('\n' + f'<p>{paragraph}</p>\n')
 
-    # Add a check to remove paragraph tags when there is a heading right after <p>
-    with open(output_filename, 'r', encoding='utf-8') as result_file:
-        result_lines = result_file.readlines()
+    # Post-process: Replace "§" with "&sect;" and "•" with "&bull;"
+    with open(output_file, 'r', encoding='utf-8') as infile:
+        content = infile.read()
 
-    with open(output_filename, 'w', encoding='utf-8') as result_file:
-        for i, line in enumerate(result_lines):
-            # Check if the line contains both <p> and <h
-            if '<p>' in line and '<h' in line:
-                # Remove <p> and </p> tags
-                line = line.replace('<p>', '').replace('</p>', '')
-
-            # Check if the line is a heading and the previous line was a paragraph
-            if line.startswith('<h') and i > 0 and result_lines[i - 1].strip().endswith('</p>'):
-                # Add a line break before the heading
-                result_file.write('\n')
-            
-            result_file.write(line)
-
-def process_line(line):
-    # Remove leading and trailing whitespaces
-    line = line.strip()
-
-    # Check if the line matches the heading pattern (number, space, text)
-    if len(line) > 1 and line[0].isdigit() and line[1] == ' ':
-        # Check if the line has § and starts with an integer/number
-        if '§' in line:
-            # It's a heading with §, treat it as <h3>
-            return f"<h3>{line}</h3>"
-        else:
-            # It's a heading without §, treat it as <h2>
-            return f"<h2>{line}</h2>"
-    else:
-        # It's a paragraph
-        return f"{line}"
+    with open(output_file, 'w', encoding='utf-8') as outfile:
+        content = content.replace('§', '&sect;').replace('•', '&bull;')
+        outfile.write(content)
 
 if __name__ == "__main__":
-    main()
+    convert_to_html("lyx.txt", "result.html")
